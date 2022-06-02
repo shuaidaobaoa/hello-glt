eb3bf8ae1962cb00aeaa0328113984803fa0c8d1
詹 <1129973600@qq.com>
2022-06-02 09:44:51 +0800

    提交消息了

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+   <el-tag class="ml-2" type="success">Tag 2</el-tag>
+
+   <el-alert title="success alert" type="success" />
+  <el-alert title="info alert" type="info" />
+  <el-alert title="warning alert" type="warning" />
+  <el-alert title="error alert" type="error" />
+</div>
+</template>
+
+
+
+
+<script>
+
+</script>
+
+<style scoped>
+.el-alert {
+  margin: 20px 0 0;
+}
+.el-alert:first-child {
+  margin: 0;
+}
+</style>
